de1abc57e897c4453cc43b47d8daf42ca59d7878
Tran Nguyen Hoai Nam <92512656+namth1793@users.noreply.github.com>
2022-09-26 16:33:29 +0700

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,7 +11,7 @@ const HeroBanner = ({heroBanner}) => {
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
         <div>
-          <Link href={`/product/$(heroBanner.product)`}>
+          <Link href={`/product/${heroBanner.product}`}>
             <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
